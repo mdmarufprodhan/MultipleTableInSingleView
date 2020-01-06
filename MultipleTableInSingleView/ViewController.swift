430aeeb4f0db65d7285c8ae2683d1bb52b1d51bf
Md Prodhan <mdmarufprodhan@labs-mac.local>
2020-01-06 15:38:10 +0600

    Initial Commit

---
+++ MultipleTableInSingleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleTableInSingleView
+//
+//  Created by Md Maruf Prodhan on 1/6/20.
+//  Copyright © 2020 Md Maruf Prodhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
