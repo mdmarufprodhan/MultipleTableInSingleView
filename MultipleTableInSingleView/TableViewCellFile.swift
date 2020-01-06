cd9fa7b4770869a78eae6592c039be9312fb87f7
Md Prodhan <mdmarufprodhan@labs-mac.local>
2020-01-06 17:08:27 +0600

    Dictionary Data Added

---
+++ MultipleTableInSingleView/TableViewCellFile.swift
@@ -0,0 +1,11 @@
+//
+//  TableViewCellFile.swift
+//  MultipleTableInSingleView
+//
+//  Created by Md Maruf Prodhan on 1/6/20.
+//  Copyright © 2020 Md Maruf Prodhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewControler : 
--- MultipleTableInSingleView/ViewController.swift
+++ MultipleTableInSingleView/ViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        
+        var dictionaryData = ["A": ["Apple","Ant"],
+                              "B": ["Mango", "Tree"],
+                              "C":["Banna", "Testy"],
+                              "D":["pineapple", "great"],
+                              "E": ["grapes","not testy"]
+                             ]
+        print(dictionaryData)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
