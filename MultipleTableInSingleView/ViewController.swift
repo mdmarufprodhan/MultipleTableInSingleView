60bfe5051f5b1047e44c67630f4d1b935945e52b
Md Prodhan <mdmarufprodhan@labs-mac.local>
2020-01-07 18:13:03 +0600

    Added Function for table view

--- MultipleTableInSingleView/ViewController.swift
+++ MultipleTableInSingleView/ViewController.swift
@@ -8,23 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var table1: UITableView!
+    @IBOutlet weak var table2: UITableView!
+    
+    var item = [String]()
+    
+    var cellIdentifier : String = "cell"
+    
+    var numberOfRows : Int = 0
+   
+    
+        var myDictionary = ["A":["Apple", "Ant"],
+                    "B":["Mango", "Tree"],
+                    "C":["Banna", "Testy"],
+                    "D":["Grapes", "Awsam"],
+                    "E":["Pineapple", "Its great"]]
+                    
         
         
-        var dictionaryData = ["A": ["Apple","Ant"],
-                              "B": ["Mango", "Tree"],
-                              "C":["Banna", "Testy"],
-                              "D":["pineapple", "great"],
-                              "E": ["grapes","not testy"]
-                             ]
-        print(dictionaryData)
         
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+     override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.{
+        table1.delegate = self
+        table1.dataSource = self
+        
+        table2.delegate = self
+        table2.dataSource = self
+
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView.tag == 1{
+          //  print("Number of Rows")
+           // print(Data)
+            numberOfRows = myDictionary.count
+            print(numberOfRows)
+            return numberOfRows
+        }
+            
+        if tableView.tag == 2{
+            //  print("Number of Rows")
+            // print(Data)
+            
+          //numberOfRows = item.count
+            numberOfRows = item.count
+            
+            return numberOfRows
+        }
+        else{
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)
+        if tableView.tag == 1{
+            cell.textLabel?.text = Array(myDictionary.keys)[indexPath.row]
+        }
+        
+        else if tableView.tag == 2{
+         cell.textLabel?.text = item[indexPath.row]
+        }
+        return cell
+        
+    }
+    
+    //Action click
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      
+        if tableView.tag == 1{
+            print(Array(myDictionary.values)[indexPath.row])
+            
+            item = Array(myDictionary.values)[indexPath.row]
+        }
+        
+        table2.reloadData()
+    }
+    
+
     }
 
-
-}
 
